99cc88e152e567463b9dd122258b377162c15b5f
GYoung <ytok1108@aol.com>
2023-12-22 21:17:24 +0900

    silq example

---
+++ exercise/arrayElements/README.md
@@ -0,0 +1,9 @@
+# Result
+
+```bash
+arrayConcatReassign.slq
+(1,2,3,4)
+
+
+
+```
---
+++ hexaprint_rust/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let hex_char = '₀';
+    let i = hex_char as u32;
+    println!("hex_char :  0x{i:x?}",);
+}
